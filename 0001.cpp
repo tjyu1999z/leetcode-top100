b6670eaf22521f212e5d1d253ad7c6f3eb821b74
tjyu1999z <tjyu1999@gmail.com>
2024-06-23 11:41:32 +0800

    add 0001.cpp

---
+++ 0001.cpp
@@ -0,0 +1,19 @@
+/*
+ *  time complexity: O(n)
+ *  space complexity: O(n)
+ */
+
+class Solution{
+    public:
+        vector<int> twoSum(vector<int> &nums, int target){
+            unordered_map<int, int> map;
+            
+            for(int i = 0; i < nums.size(); i++){
+                if(map.count(target - nums[i]))
+                    return {i, map[target - nums[i]]};
+                map[nums[i]] = i;
+            }
+            
+            return {};
+    }
+};
